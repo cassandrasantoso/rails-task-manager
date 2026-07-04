07598fde791dbbe57514fff9b208e57d176812e7
Cassandra Santoso <cassandra.santoso04@gmail.com>
2026-07-04 14:27:38 +0900

    tasks basic function

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,8 @@ Rails.application.routes.draw do
 
   root "tasks#index"
 
+  resources :tasks
+
   get    "tasks",          to: "tasks#index",  as: :tasks
   get    "tasks/:id",      to: "tasks#show",   as: :task
   get    "tasks/new",      to: "tasks#new",    as: :new_task
